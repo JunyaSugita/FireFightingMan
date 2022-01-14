a558c28e85e8f469a79194be3fb3d6a05c34b070
RyoutaKaneko <k021g1476@g.neec.ac.jp>
2022-01-14 13:05:09 +0900

    パーティクルの関数を発生、移動、描画に分割

--- Particle.cpp
+++ Particle.cpp
@@ -31,12 +31,9 @@ Particle::~Particle() {
 	delete player;
 }
 
-//ˆÚ“®
-void Particle::Move(int x, int y, int scroll, int fireR) {
+void Particle::Emit(int x, int y, int fireR) {
 
-	Draw(scroll);
-
-	for (int j = 0; j < 10; j++) {
+	for (int j = 0; j <10; j++) {
 		for (int i = 0; i < PARTICLE_CONST; i++) {
 			if (particle[i].isEmit == 0) {
 				particle[i].isEmit = 1;
@@ -47,6 +44,11 @@ void Particle::Move(int x, int y, int scroll, int fireR) {
 			}
 		}
 	}
+
+}
+
+//ˆÚ“®
+void Particle::Move() {
 
 	for (int i = 0; i < PARTICLE_CONST; i++) {
 		if (particle[i].isEmit == 1) {
