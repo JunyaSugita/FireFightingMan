83d9e92f074c0515ecde4378d309b18b80e4cd26
JunyaSugita <k021g1293@g.neec.ac.jp>
2022-01-31 09:32:26 +0900

    空中でジャンプできるバグ、空中で足音が出るバグを修正

--- Player.cpp
+++ Player.cpp
@@ -129,17 +129,27 @@ void Player::PlayerMove(int LInputX, int RInputX, int RInputY, int isRescued) {
 	if (LInputX > 0 || LInputX < 0) {
 		player.transform.x += (LInputX / 200) * speed;
 
-		if (speed == 0.8f) {
-			if (CheckSoundMem(walkSE) == false) {
-				PlaySoundMem(walkSE, DX_PLAYTYPE_BACK, true);
+		if (player.isJump == false) {
+			if (speed == 0.8f) {
+				if (CheckSoundMem(walkSE) == false) {
+					PlaySoundMem(walkSE, DX_PLAYTYPE_BACK, true);
+				}
+				if (CheckSoundMem(dashSE) == true) {
+					StopSoundMem(dashSE);
+				}
 			}
-			if (CheckSoundMem(dashSE) == true) {
-				StopSoundMem(dashSE);
+			else if (speed == 1.5f) {
+				if (CheckSoundMem(dashSE) == false) {
+					PlaySoundMem(dashSE, DX_PLAYTYPE_BACK, true);
+				}
+				if (CheckSoundMem(walkSE) == true) {
+					StopSoundMem(walkSE);
+				}
 			}
 		}
-		else if (speed == 1.5f) {
-			if (CheckSoundMem(dashSE) == false) {
-				PlaySoundMem(dashSE, DX_PLAYTYPE_BACK, true);
+		else {
+			if (CheckSoundMem(dashSE) == true) {
+				StopSoundMem(dashSE);
 			}
 			if (CheckSoundMem(walkSE) == true) {
 				StopSoundMem(walkSE);
@@ -182,7 +192,7 @@ void Player::PlayerMove(int LInputX, int RInputX, int RInputY, int isRescued) {
 
 void Player::PlayerJump(int pad, int isRescued, int map[][50]) {
 	if (map[leftBottomY][leftBottomX] != BLOCK && map[rightBottomY][rightBottomX] != BLOCK) {
-		player.isJump == true;
+		player.isJump = true;
 	}
 	if (pad & PAD_INPUT_5) {
 		if (isButton == 0 && player.isJump == false) {
